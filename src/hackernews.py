4b98a0bc861bfb9f1da380f1fc812115f1bf0e4b
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2021-04-20 23:47:24 -0400

    Trigger terminating istio-proxy after finish

--- src/hackernews.py
+++ src/hackernews.py
@@ -3,12 +3,12 @@ import requests, json
 from kafka import KafkaProducer
 import logging
 import sys
+import time
 from prometheus_client import (
   push_to_gateway,
   CollectorRegistry,
   Summary,
 )
-
 
 # set log
 logger = logging.getLogger("topstories-logger")
@@ -17,6 +17,13 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 # init kafka producer
 kafka_server = os.environ.get('KAFKA_HOST') + ':' + os.environ.get('KAFKA_PORT')
 producer = KafkaProducer(bootstrap_servers=kafka_server)
+
+# test if Istio SideCar is ready
+while requests.get('http://localhost:15021/healthz/ready').status_code != 200:
+  logger.info("Waiting for SideCar get ready...")
+  time.sleep(1)
+
+logger.info("SideCar is running, start getting stories")
 
 # Create a metric to track time spent and requests made.
 PUSH_GATEWAY = "http://prometheus-prometheus-pushgateway.default:9091"
@@ -46,3 +53,8 @@ if __name__ == '__main__':
     send_id_to_kafka(id)
   # push metrics to pushgateway
   push_to_gateway(PUSH_GATEWAY, job='topstories_cronjob', registry=REGISTRY)
+
+  logger.info("Getting stories completed")
+  # Stop Istio SideCar
+  res = requests.post('http://localhost:15020/quitquitquit')
+  logger.info("Stopping SiceCar: " + res.text)
